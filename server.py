d5d25c288985ed71688c44a2826655d8031845dc
ImNotWallin <terris.thompson@yahoo.com>
2021-09-12 22:21:10 -0400

    my portfolio website

---
+++ server.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, send_from_directory, url_for, request, redirect
+import os
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+@app.route('/<page_name>')
+def html_page(page_name='index.html'):
+  return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'Something went wrong.'
+
+
+    # if request.method == 'POST':
+    #     return do_the_login()
+    # else:
+    #     return show_the_login_form()
+
+# @app.route('/')
+# def my_home():
+#     return render_template('index.html')
+
+# @app.route('/<string:page_name>')
+# def html_page(page_name):
+#     return render_template('page_name')
+
+# @app.route('/works.html')
+# def works():
+#     return render_template('works.html')
+
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
+
+
+
+
+# @app.route('/blog')
+# def blog():
+#     return 'Welcome to the Eye of the Storm'
+
+
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),
+#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# # <link rel="shortcut icon" href="{{ url_for('static', filename='favicon.ico') }}">
+#
